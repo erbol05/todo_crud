c70d17c5c322f016abdfcb1e5d8a210202397db8
erbol <sujutbekoverbol031@gmail.com>
2024-11-13 19:14:34 +0600

    ..

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -8,20 +8,29 @@ const Welcome = () => {
   const [univer, setUniver] = useState("");
   const [product, setProduct] = useState([]);
   const postFunc = async () => {
-    const newe = {
-      name: name,
-      phone: phone,
-      prof: prof,
-      univer: univer,
-    };
-    await axios.post(
-      "https://api.elchocrud.pro/api/v1/f31efbeba9b7ed8753457db75fed4f48/swa",
-      newe
-    );
-    setName("");
-    setPhone("");
-    setProf("");
-    setUniver("");
+    if (
+      name.trim() == "" ||
+      phone.trim() == "" ||
+      prof.trim() == "" ||
+      univer.trim() == ""
+    ) {
+      alert("толук толтур");
+    } else {
+      const newe = {
+        name: name,
+        phone: phone,
+        prof: prof,
+        univer: univer,
+      };
+      await axios.post(
+        "https://api.elchocrud.pro/api/v1/f31efbeba9b7ed8753457db75fed4f48/swa",
+        newe
+      );
+      setName("");
+      setPhone("");
+      setProf("");
+      setUniver("");
+    }
   };
   const getProduct = async () => {
     const { data } = await axios.get(
@@ -76,7 +85,9 @@ const Welcome = () => {
                 <h2>
                   кесиби: <span>{el.prof}</span>
                 </h2>
-                <h3>окуусу: <span>{el.univer}</span></h3>
+                <h3>
+                  окуусу: <span>{el.univer}</span>
+                </h3>
               </div>
             ))}
           </div>
